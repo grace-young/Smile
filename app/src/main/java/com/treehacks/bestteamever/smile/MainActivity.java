7b08437122aa2cf6d2ce7f648a8c784e0494c6de
Catherina Xu <catherinaxu@stanford.edu>
2016-02-13 15:41:05 -0800

    tone stuff works!!!

--- app/src/main/java/com/treehacks/bestteamever/smile/MainActivity.java
+++ app/src/main/java/com/treehacks/bestteamever/smile/MainActivity.java
@@ -1,7 +1,12 @@
 package com.treehacks.bestteamever.smile;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToneTask toneTask = new ToneTask();
+        toneTask.execute();
+    }
+
+    private class ToneTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
+            service.setUsernameAndPassword("dbd0722f-64ce-4d9e-904c-01ae273be401", "q4nNXEbBFQ8B");
+
+            String text = "I know the times are difficult! Our sales have been "
+                    + "disappointing for the past three quarters for our data analytics "
+                    + "product suite. We have a competitive data analytics product "
+                    + "suite in the industry. But we need to do our job selling it! "
+                    + "We need to acknowledge and fix our sales challenges.";
+
+            ToneAnalysis tone = service.getTone(text);
+            Log.d("debug", tone.toString());
+
+            return true;
+        }
+
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if (result) {
+                Log.d("debug", "Congrats, Catherina!");
+            }
+        }
     }
 }
+
