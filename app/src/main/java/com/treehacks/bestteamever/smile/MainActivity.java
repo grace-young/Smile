91ea8a9fce0770bce9935f3a3edf5a50b4b44ecc
Catherina Xu <catherinaxu@stanford.edu>
2016-02-13 17:58:24 -0800

    aggregated results into map

--- app/src/main/java/com/treehacks/bestteamever/smile/MainActivity.java
+++ app/src/main/java/com/treehacks/bestteamever/smile/MainActivity.java
@@ -1,14 +1,20 @@
 package com.treehacks.bestteamever.smile;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    private static HashMap<String, Double> sentimentMap = new HashMap<String, Double>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +39,33 @@ public class MainActivity extends AppCompatActivity {
                     + "We need to acknowledge and fix our sales challenges.";
 
             ToneAnalysis tone = service.getTone(text);
-            Log.d("debug", tone.toString());
+            try {
+                JSONObject toneJSON = new JSONObject(tone.toString());
+                JSONArray toneCategories = toneJSON.getJSONObject("document_tone").getJSONArray("tone_categories");
+
+                JSONArray emotionTones = toneCategories.getJSONObject(0).getJSONArray("tones");
+
+                for (int i = 0; i < emotionTones.length(); i++) {
+                    sentimentMap.put(emotionTones.getJSONObject(i).getString("tone_name"), emotionTones.getJSONObject(i).getDouble("score") * 100);
+                }
+
+                JSONArray writingTones = toneCategories.getJSONObject(1).getJSONArray("tones");
+
+                for (int i = 0; i < writingTones.length(); i++) {
+                    sentimentMap.put(writingTones.getJSONObject(i).getString("tone_name"), emotionTones.getJSONObject(i).getDouble("score") * 100);
+                }
+
+                JSONArray socialTones = toneCategories.getJSONObject(2).getJSONArray("tones");
+
+                for (int i = 0; i < socialTones.length(); i++) {
+                    sentimentMap.put(socialTones.getJSONObject(i).getString("tone_name"), emotionTones.getJSONObject(i).getDouble("score") * 100);
+                }
+
+                Log.d("debug", sentimentMap.toString());
+
+            } catch(Exception JSONException) {
+                Log.d("debug", "JSON is not correctly formatted!");
+            }
 
             return true;
         }
